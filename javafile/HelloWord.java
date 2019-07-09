7e43a176ed400260c518ffc951f03039818918d7
litongtong <2542469086@qq.com>
2019-07-09 13:53:27 +0800

    Merge branch 'master' of https://github.com/puppet16/puppet16.github.io
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- javafile/HelloWord.java
+++ javafile/HelloWord.java
@@ -1,5 +1,29 @@
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int[] a= new int[]{1,2,3,4,5,6};
+        int[] b = cal(a);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println("index:"+i+" value:"+b[i]);
+        }
+    }
+
+    private static int[] cal(int[] arr) {
+            int[] result = new int[arr.length];
+            result[0] = 1;
+            if(arr.length == 0) {
+                return result;
+            } else if(arr.length!=0){
+                for(int i = 1;i<arr.length;++i){
+                    result[i] = result[i-1]*arr[i-1];
+                }
+                int tmp = 1;
+                for(int j = arr.length-2;j>=0;j--){
+                    tmp = tmp*arr[j+1];
+                    result[j] = result[j]*tmp;
+                }
+            }
+            return result;
     }
 }
+
